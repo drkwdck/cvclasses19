01bf1699d5d219913a610ed55485fed5b08e2c38
EReshetnikov <ereshetnikov@rockitsoft.ru>
2023-11-08 00:13:35 +0300

    lab 2

--- cvlib/src/select_texture.cpp
+++ cvlib/src/select_texture.cpp
@@ -30,6 +30,17 @@ struct descriptor : public std::vector<double>
         }
         return res;
     }
+
+    double norm_l2() const
+    {
+        double res = 0.0;
+        for (auto v : *this)
+        {
+            res += v * v;
+        }
+        return cv::sqrt(res);
+    }
+
 };
 
 void calculateDescriptor(const cv::Mat& image, int kernel_size, descriptor& descr)
@@ -37,13 +48,11 @@ void calculateDescriptor(const cv::Mat& image, int kernel_size, descriptor& desc
     descr.clear();
     const double th = CV_PI / 4;
     const double lm = 10.0;
-    const double gm = 0.5;
+    const double gm = 0.75;
     cv::Mat response;
     cv::Mat mean;
     cv::Mat dev;
 
-    // \todo implement complete texture segmentation based on Gabor filters
-    // (find good combinations for all Gabor's parameters)
     for (auto sig = 5; sig <= 15; sig += 5)
     {
         cv::Mat kernel = cv::getGaborKernel(cv::Size(kernel_size, kernel_size), sig, th, lm, gm);
@@ -53,15 +62,14 @@ void calculateDescriptor(const cv::Mat& image, int kernel_size, descriptor& desc
         descr.emplace_back(dev.at<double>(0));
     }
 }
-} // namespace
+}
 
 namespace cvlib
 {
 cv::Mat select_texture(const cv::Mat& image, const cv::Rect& roi, double eps)
 {
     cv::Mat imROI = image(roi);
-
-    const int kernel_size = std::min(roi.height, roi.width) / 2; // \todo round to nearest odd
+    const int kernel_size = std::min(roi.height, roi.width) / 4 * 2 + 1;
 
     descriptor reference;
     calculateDescriptor(image(roi), kernel_size, reference);
@@ -72,18 +80,17 @@ cv::Mat select_texture(const cv::Mat& image, const cv::Rect& roi, double eps)
     cv::Rect baseROI = roi - roi.tl();
 
     // \todo move ROI smoothly pixel-by-pixel
-    for (int i = 0; i < image.size().width / roi.width; ++i)
+    for (int i = 0; i < image.size().width - roi.width; ++i)
     {
-        for (int j = 0; j < image.size().height / roi.height; ++j)
+        for (int j = 0; j < image.size().height - roi.height; ++j)
         {
-            auto curROI = baseROI + cv::Point(roi.width * i, roi.height * j);
+            auto curROI = baseROI + cv::Point(i, j);
             calculateDescriptor(image(curROI), kernel_size, test);
-
             // \todo implement and use norm L2
-            res(curROI) = 255 * ((test - reference).norm_l1() <= eps);
+            res(curROI) = 255 * ((test - reference).norm_l2() <= eps);
         }
     }
 
     return res;
 }
-} // namespace cvlib
+}
